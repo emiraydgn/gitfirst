e088ff3f3e29ad043b08706bcf9e9817e22ac97f
emiraydgn <emiraydogan03@gmail.com>
2025-10-21 11:50:02 +0300

    Update gittask_Эмир_Айдоган_513090450004.cpp

--- gittask_Эмир_Айдоган_513090450004.cpp
+++ gittask_Эмир_Айдоган_513090450004.cpp
@@ -1,15 +1,23 @@
-int** convert(const int* t, size_t n, const size_t* lns, size_t rows) {
-    size_t need = 0;
-    for (size_t i = 0; i < rows; ++i) need += lns[i];
-    if (need > n) return nullptr;
+int** convert(const int* t, size_t n, const size_t* lns, size_t rows)
+{
+    size_t total = 0;
+    for (size_t i = 0; i < rows; i++)
+        total += lns[i];
 
-    int** res = new int*[rows];
-    size_t k = 0;
-    for (size_t i = 0; i < rows; ++i) {
-        res[i] = new int[lns[i]];
-        for (size_t j = 0; j < lns[i]; ++j)
-            res[i][j] = t[k++];
+    if (total > n)
+        return nullptr;
+
+    int** arr = new int*[rows];
+    size_t index = 0;
+
+    for (size_t i = 0; i < rows; i++) {
+        arr[i] = new int[lns[i]];
+        for (size_t j = 0; j < lns[i]; j++) {
+            arr[i][j] = t[index];
+            index++;
+        }
     }
-    return res;
+
+    return arr;
 }
 
